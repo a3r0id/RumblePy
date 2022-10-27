d078ca75b53477b9b0dcf49782b2f0e335def44a
hostinfodev <50676042+hostinfodev@users.noreply.github.com>
2022-10-26 23:37:12 -0700

    0.0.4

--- rumble_bot/methods/feeds/subscriptions.py
+++ rumble_bot/methods/feeds/subscriptions.py
@@ -12,32 +12,6 @@ def subscriptions(self, page=0):
     
     feed = []
     
-    # parse the video info
-    """
-    <article class="video-item">
-        <h3 class="video-item--title">Red Tsunami - In the Litter Box w/ Jewels &amp; Catturd 10/19/2022 - Ep. 192</h3><a
-            class="video-item--a"
-            href="/v1or4u1-red-tsunami-in-the-litter-box-w-jewels-and-catturd-10192022-ep.-192.html"><img
-                class="video-item--img"
-                src="https://sp.rmbl.ws/s8/1/5/0/q/f/50qfg.oq1b-small-Red-Tsunami-In-the-Litter-B.jpg"
-                alt="Red Tsunami - In the Litter Box w/ Jewels &amp; Catturd 10/19/2022 - Ep. 192"></a>
-        <footer class="video-item--footer ellipsis-1">
-            <address class="video-item--by"><a rel="author" class="video-item--by-a video-item--by-a--c1313924"
-                    href="/c/InTheLitterBox">
-                    <div class="ellipsis-1">In The Litter Box w/ Jewels &amp; Catturd<svg
-                            class="video-item--by-verified verification-badge-icon" width="24" height="24"
-                            viewBox="0 0 24 24">
-                            <circle cx="12" cy="12" r="12"></circle>
-                            <path fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10"
-                                d="M5.4 11.1l5 5 8.2-8.2"></path>
-                        </svg></div>
-                </a></address><span class="video-item--duration" data-value="1:07:19"></span><span
-                class="video-item--meta video-item--views" data-value="6,608"></span><span
-                class="video-item--meta video-item--rumbles" data-value="487"></span><time
-                class="video-item--meta video-item--time" datetime="2022-10-19T12:26:47-04:00">Oct 19</time>
-        </footer>
-    </article>
-    """
     for article in scraper.soup.find_all("article", class_="video-item"):
         # get the video title
         title = article.find("h3", class_="video-item--title").text
